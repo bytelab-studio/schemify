66e6db9c64cc281fd2764bb6a23472a1403b07af
Christoph Koschel <christoph.koschel86@gmail.com>
2025-08-10 10:23:59 +0200

    Implement CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
     test: {
         environment: 'node',
         coverage: {
-            provider: "v8"
+            provider: "v8",
+            reporter: ["html", "lcov"],
         }
     },
 });
